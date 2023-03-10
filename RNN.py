c1a29c1f9c437f8d5906238979fe3be3e0f51df9
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-03-10 02:30:27 -0300

    Create RNN.py
    gerando dados sintéticos com 1000 linhas e 50 colunas usando a função np.random.randn. Estamos criando uma rede neural recorrente com uma camada LSTM com 32 neurônios e uma camada de saída com um neurônio e função de ativação sigmoidal.
    
    Estamos compilando a rede neural usando a função de perda 'binary_crossentropy' e o otimizador 'adam', e estamos acompanhando a precisão como uma métrica.
    
    Estamos dividindo os dados em treinamento e teste e estamos preparando os dados de entrada e saída usando a função prepare_data. Estamos treinando a rede neural com os dados de treinamento e avaliando sua precisão usando os dados de teste.
    
    Observe que os dados de treinamento e teste precisam ser definidos antes de treinar e avaliar a rede neural, e que os dados de entrada para uma rede neural recorrente precisam ser de três dimensões (amostras x tempo x características).

---
+++ RNN.py
@@ -0,0 +1,36 @@
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+import numpy as np
+
+# Gerando dados sintéticos
+data = np.random.randn(1000, 50)
+
+# Criando a rede neural
+model = Sequential()
+model.add(LSTM(32, input_shape=(None, 50)))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compilando a rede neural
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Dividindo os dados em treinamento e teste
+train_data = data[:800]
+test_data = data[800:]
+
+# Preparando os dados de entrada e saída
+def prepare_data(data):
+    X = data[:, :-1]
+    y = data[:, -1]
+    X = np.reshape(X, (X.shape[0], 1, X.shape[1]))
+    return X, y
+
+X_train, y_train = prepare_data(train_data)
+X_test, y_test = prepare_data(test_data)
+
+# Treinando a rede neural
+model.fit(X_train, y_train, epochs=10, batch_size=32)
+
+# Avaliando a rede neural
+loss, accuracy = model.evaluate(X_test, y_test)
+print('Loss:', loss)
+print('Accuracy:', accuracy)
